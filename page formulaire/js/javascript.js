6ec786263c062f3d36931a77f0cfcb670d8d5832
Adam1832 <159237282+Adam1832@users.noreply.github.com>
2024-02-22 14:11:47 +0100

    Delete page formulaire directory

--- page formulaire/js/javascript.js
+++
@@ -1,8 +0,0 @@
-function addToCart() {
-    var produit = document.getElementById('produit').value;
-
-    var cartItems = document.getElementById('cart-items');
-    var listItem = document.createElement('li');
-    listItem.textContent = produit + ' x ';
-    cartItems.appendChild(listItem);
-}
